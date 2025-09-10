8a62c7a59728ee7f99e28c7ac8bc195998e18148
aryanParth-afk <aryan10c112077@gmail.com>
2025-09-11 00:42:08 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'anti-india-detector-v2.0.0';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/app.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
